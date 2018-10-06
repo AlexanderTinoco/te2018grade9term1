7ab0c3a327e37d00fac6a99a2783a3656e31aa55
Alexander <Alexandera@csedge.org>
2018-10-06 13:18:12 -0700

    updated data.js

--- data.js
+++ data.js
@@ -1,5 +1,14 @@
 // Example
-let message = "Hello World!";
+let message = "If fire is hot can it be cold? ";
 console.log(message);
-
+let bDayMonth = 3
+let agee = 14
+let population = 100
+let people = "Hurt me"
+let sound = "screech"
+let meBurning = false
+let fortniteIsBad = true
+let meHaveNoHobby = false
+let me;
+console.log(me)
 // Type your code below!
